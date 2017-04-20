03ba6d7ec8fb2cda291ef2bf3d858f1f8a2dcd45
HAo <hao.chen3@uq.net.au>
2017-04-20 10:31:42 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+bitcoinplus (XBC)
+
+bitcointalk.org/index.php?topic=714606.0
+
+www.bitcoinplus.org
+
+===================================
+
+Ports 8885 & RPC 8884
+
+Testnet 18885 & RPC 18884
---
+++ src/genesis.h
@@ -0,0 +1,15 @@
+#ifndef GENESIS_H
+#define GENESIS_H
+
+#include "bignum.h"
+
+// Genesis block 0 hash
+
+static const uint256 hashGenesisBlock("0000030492f2d16e591aeac762163e692b2c6244a56df24cedc945907bef6719");
+static const uint256 hashGenesisBlockTestNet("0000030492f2d16e591aeac762163e692b2c6244a56df24cedc945907bef6719");
+static const uint256 CheckPointBlock1("191e54d44ed16d6cb90f5aeb374098d915b93ef65b26423720da09a7ffe7e84c"); // checkpoint at block 100,000
+
+// TODO: Move Kernel checksum in here
+
+
+#endif // GENESIS_H
